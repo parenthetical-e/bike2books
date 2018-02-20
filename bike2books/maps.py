8f976470a4c41a7b9e19149cb6fde8f717c43dae
Erik Peterson <Erik.Exists@gmail.com>
2018-02-20 00:01:22 -0800

    Map API complete

--- bike2books/maps.py
+++ bike2books/maps.py
@@ -1,3 +1,8 @@
+import numpy as np
+import pandas as pd
+import gmaps
+
+
 def heatmap(locations, times):
-    """Create a heatmap, overlaid on GoogleMap of travel times."""
+    """Create a heatmap of travel times, overlaid on a Google Map."""
     pass
