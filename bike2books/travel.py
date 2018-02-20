893a62e0ec33d70ebdb8af8fec65ecb7058f4fff
Erik Peterson <Erik.Exists@gmail.com>
2018-02-20 10:41:28 -0800

    Added neighborhood data, and lookup fns

--- bike2books/travel.py
+++ bike2books/travel.py
@@ -2,11 +2,17 @@ import os
 import urllib
 import geojson
 import bike2books
+import pandas as pd
+
+
+def _get_dat_path():
+    base_path = os.path.split(bike2books.__file__)[0]
+    data_path = os.path.join(base_path, "data")
+    return data_path
 
 
 def _get_data():
-    base_path = os.path.split(bike2books.__file__)[0]
-    data_path = os.path.join(base_path, "data")
+    data_path = _get_dat_path()
 
     if not os.path.isdir(data_path):
         # Create data dir
@@ -27,12 +33,55 @@ def _get_data():
         # Parking locations
         file_name = os.path.join(data_path,
                                  "treas_parking_meters_loc_datasd.csv")
-        url = "http://seshat.datasd.org/parking_meters/treas_meters_2017_pole_by_month_datasd.csv"
+        url = "http://seshat.datasd.org/parking_meters/treas_parking_meters_loc_datasd.csv"
+        urllib.urlretrieve(url, file_name)
 
         # Bike paths
         file_name = os.path.join(data_path, "bike_routes_datasd.geojson")
         url = "http://seshat.datasd.org/sde/bike_route/bike_routes_datasd.geojson"
         urllib.urlretrieve(url, file_name)
+
+
+def neighborhood_location(name):
+    nns = neighborhoods()
+    return nns[name]
+
+
+def neighborhood_names():
+    nns = neighborhoods()
+    return nns.keys()
+
+
+def neighborhoods():
+    """Neighborhood names and centers.
+
+    Note: Names take from from the Parking meter data at:
+    http://seshat.datasd.org/parking_meters/treas_parking_meters_loc_datasd.csv
+
+    Location taken from wikipedia, or manual estimates derived via Google Maps.
+    (This should be automated in the future).
+    """
+
+    return {
+        'Barrio Logan': (32.697539, -117.142025),
+        'Midtown': (32.738611, -117.175278),
+        'Mission Beach': (32.775034, -117.251903),
+        'Mission Hills': (32.752828, -117.186361),
+        'Point Loma': (32.67, -117.241944),
+        'Core - Columbia': (32.716678, -117.168244),
+        'Cortez Hill': (32.721667, -117.16),
+        'East Village': (32.724167, -117.167222),
+        'Gaslamp': (32.711667, -117.159167),
+        'Little Italy': (32.724167, -117.167222),
+        'Marina': (32.711667, -117.169167),
+        'Golden Hill': (32.718, -117.134),
+        'North Park': (32.740831, -117.129719),
+        'Talmadge': (32.764, -117.09),
+        'University Heights': (32.76, -117.14),
+        'Bankers Hill': (32.731, -117.164),
+        'Five Points': (32.7431, -117.1848),
+        'Hillcrest': (32.75, -117.166667)
+    }
 
 
 def nearest_library(location):
