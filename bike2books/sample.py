e89299a60b05a28dcbf6885aca83601e9d0e48c2
Erik Peterson <Erik.Exists@gmail.com>
2018-02-19 23:34:32 -0800

    Crude sampling function complete

--- bike2books/sample.py
+++ bike2books/sample.py
@@ -1,8 +1,29 @@
-def draw(radius):
-    """Draw a sample, starting from the city center."""
-    pass
+import numpy as np
+
+# City center
+CENTER = (32.7157, 117.1611)
 
 
-def nearest_library(lat, long):
-    """Nearest library, in miles"""
-    pass
+def sample_location(radius=6, prng=None):
+    """Draw a sample, starting from the city center.
+    
+    Parameters
+    ---------
+    radius : numeric
+        Radius of neighborhoods to sample (in miles).
+    prng : None, np.random.RandomState
+        Control random seeding in a sensible way.
+    """
+
+    # Handle random seed
+    if prng is None:
+        prng = np.random.RandomState()
+
+    # Take sample
+    r = prng.uniform(0, radius / 66.9)  # Crude conversion
+    theta = prng.uniform(0, 2 * np.pi)
+
+    x = r * np.cos(theta)
+    y = r * np.sin(theta)
+
+    return (CENTER[0] + x, CENTER[1] + y)
